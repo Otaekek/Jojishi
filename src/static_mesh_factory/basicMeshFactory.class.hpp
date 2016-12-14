804134fe3402e7b5fa419ea00fbd534b4fe7d94a
Rodolphe CARGOU <rcargou@e1r12p9.42.fr>
2016-12-14 11:42:05 +0100

    ups

---
+++ src/static_mesh_factory/basicMeshFactory.class.hpp
@@ -0,0 +1,46 @@
+#ifndef BASICMESHFACTORY
+#define BASICMESHFACTORY
+
+#include <transform.class.hpp>
+#include <renderBuiltIn.class.hpp>
+#include <fileLoader.class.hpp>
+#include <staticMemoryManager.class.hpp>
+
+#define MESH_FACTORY_MAX_ELEM 4096
+
+class staticMesh {
+
+public:
+
+	staticMesh();
+	staticMesh(glm::vec3 pos, glm::vec3 rot, float angle, glm::vec3 scale, std::string meshName);
+	~staticMesh();
+
+	void update();
+
+private:
+
+	uint32_t	transformHandler;
+	uint32_t	assetHandler;
+	uint32_t	renderGOhandler;
+
+};
+
+class staticMeshManager {
+
+public:
+
+	static void		init();
+	static void		shutdown();
+
+	static void		create(glm::vec3 pos, glm::vec3 rot, float angle, glm::vec3 scale, std::string meshName);
+
+	static void		update();
+
+private:
+
+	static uint32_t		numElem;
+	static staticMesh	elems[MESH_FACTORY_MAX_ELEM];
+};
+
+#endif
