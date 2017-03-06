e76a2280d62682973fdda0e238c79f6d003d19bd
Rodolphe CARGOU <rcargou@e1r12p11.42.fr>
2017-03-06 16:03:30 +0100

    entity

--- src/entitySystem/entity.class.hpp
+++ src/entitySystem/entity.class.hpp
@@ -3,33 +3,35 @@
 
 #define MAX_COMPONENT 12
 
+enum e_qtype {
+	E_SCRIPT,
+	E_RENDER,
+	E_CAMERA,
+	E_TERRAIN,
+	E_LIGHT,
+	E_SIZEQ
+};
+
 class entity {
 
 public:
+
+	entity();
+	~entity();
 
 	void		set_index(uint32_t index) {_manager_index = index;}
 	uint32_t	get_index() {return _manager_index;}
 	void		update();
 
+	void del(uint32_t q, uint32_t handler);
+	void push(uint32_t q, uint32_t handler);
+
 private:
 
+	uint32_t	_queues[E_SIZEQ][MAX_COMPONENT];
+	uint32_t	_counts[E_SIZEQ];
+
 	uint32_t	_manager_index;
-
-	uint32_t	_scriptsHandlers[MAX_COMPONENT];
-	uint32_t	_scriptsCount;
-
-	uint32_t	_rendersHandlers[MAX_COMPONENT];
-	uint32_t	_renderCount;
-
-	uint32_t	_terrainsHandlers[MAX_COMPONENT];
-	uint32_t	_terrainCount;
-
-	uint32_t	_lightsHandlers[MAX_COMPONENT];
-	uint32_t	_lightCount;
-
-	uint32_t	_camerasHandlers[MAX_COMPONENT];
-	uint32_t	_cameraCount;
-
+	uint32_t	_transform;
 }
-
 #endif 
